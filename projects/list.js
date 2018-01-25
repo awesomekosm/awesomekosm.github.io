1d58d336fa63879bfdddd2f48d2ea5f386917791
Eugene Barnett <destrachan2030@gmail.com>
2018-01-24 23:44:24 -0500

    Added project section
    -renderProjectList
    --takes a script template and generates
    --a list of projects from json list in
    --list.js
    --Done in case I move this project to
    --be a deployable with services serving
    --json

---
+++ projects/list.js
@@ -0,0 +1,75 @@
+var sudokuSolver = {
+    href: 'https://github.com/Z-EMB/SudokuSolver',
+    imageLink: 'projects/ss.png',
+    title: 'Sudoku Solver',
+    description: 'Iterating over a game board and applying strategies for solving the puzzle <a href="projects/SudokuSolverEXE.zip" style="color:blue">Download Binary Win32</a>'
+};
+
+var decisionTree = {
+    href: 'https://github.com/Z-EMB/DecisionTree',
+    imageLink: 'projects/dt.png',
+    title: 'Decision Tree',
+    description: 'Reads behavior tree from a custom XML, builds in-memory representation and provides a method by which user can input an event and the program will respond with appropriate response. <a href="projects/DecisionTree.zip" style="color:blue">Download Binary Win32</a>'
+};
+
+var connect4 = {
+    href: 'https://github.com/Z-EMB/Connect4',
+    imageLink: 'projects/connect4.png',
+    title: 'Connect 4',
+    description: 'Min-Max algorithm to play a connect 4 game, human vs ai or human vs human. Input width, height, #in row to win, and time to run recursion (ignored) <a href="projects/Connect4EXE.zip" style="color:blue">Download Binary Win32</a>'
+};
+
+var ann = {
+    linkStyle: 'pointer-events: none;',
+    imageLink: 'projects/ann.png',
+    title: 'Number Recognition ANN',
+    description: 'Forward and back propagate to adjust the weight for the training phase on 8x8 bitmap numbers 0-9. Forward propagate arbitrary 8x8 bitmap character to receive a guess after the training is complete. Training has parameters such as number of input nodes, output nodes, hidden layers, size of each hidden layer, a learning constant'
+};
+
+var gridworldSarsa = {
+    href: 'https://github.com/Z-EMB/SARSA-Learning',
+    imageLink: 'projects/gridworld.png',
+    title: 'Grid World SARSA Learning',
+    description: 'World 20x20, a mouse, walls, and a reward. The mouse moves not based on memory of old moves but rather on environmental hints (weights) that are adjusted at each episode(mouses attempt to get to the reward). <a href="projects/GridworldJAR.zip" style="color:blue">Download Grid World Jar</a> <iframe height="175" src="https://www.youtube.com/embed/3F7OTI1TB_k" frameborder="0" style="display: inherit; margin-top:1em" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+};
+
+var chat = {
+    href: 'https://github.com/Z-EMB/Chat',
+    imageLink: 'projects/chat.png',
+    title: 'WebSocket Chat',
+    description: 'A single-page webchat application with file sharing. Using socket io, vanilla js, Node.js. Worked on with friends, Felipe and Mike'
+};
+
+var trendrurer = {
+    linkStyle: 'pointer-events: none;',
+    imageLink: 'projects/trendrurer.png',
+    title: 'TRENDRURER <span style="color:red">Maintenance</span>',
+    description: 'News aggregator at http://trendrurer.com/ Built with spring boot'
+};
+
+var multisession  = {
+    href: 'https://bitbucket.org/TotallyEMB/redismultiwarsession',
+    imageLink: 'projects/redissession.png',
+    title: 'Multi Server single session',
+    description: 'Sharing a session while running many apps on different servers, session synchronization with https://redis.io/ and spring framework. Users navigate between apps seamlessly, no sso.'
+};
+
+var rogatka = {
+    linkStyle: 'pointer-events: none;',
+    imageLink: 'projects/untitled.png',
+    title: 'Rogatka Games',
+    description: 'Making pieces to put together a networked 3D game. Current progress - OpenGL renderer for truetype and environment, model loaders, memory management, input, simulated timing, sqlite integration, lua integration.'
+};
+
+var projectList = [
+    rogatka,
+    multisession,
+    trendrurer,
+    chat,
+    gridworldSarsa,
+    ann,
+    connect4,
+    decisionTree,
+    sudokuSolver
+];
+
